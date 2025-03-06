d03f581fbbe500f43c2288a1bf7508b75412bff5
Taha Shahid <90358100+tahas02@users.noreply.github.com>
2025-03-06 14:44:34 -0500

    Added DB connection

--- src/DBConnect/DBConnect.py
+++ src/DBConnect/DBConnect.py
@@ -1,2 +1,15 @@
 import pymongo
 
+client = pymongo.MongoClient('mongodb://localhost/cdm-data')
+
+db = client['cdm-data']
+
+db_collections = db['cdms']
+
+i = 0
+for document in db_collections.find():
+    if(i < 3):
+        print(document)
+        i += 1
+    else:
+        break
