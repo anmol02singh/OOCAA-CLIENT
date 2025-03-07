bbb85f9d599e5701d100c95ca415c6cf92adbab6
Taha Shahid <90358100+tahas02@users.noreply.github.com>
2025-03-07 15:49:48 -0500

    KAN-56: restructuring files

--- src/models/cdm.py
+++ src/models/cdm.py
@@ -1,4 +1,33 @@
-from mongoengine import *
+from pydantic import BaseModel
+from datetime import datetime, date
 
-class CDM(Document):
-    
+class CDM(BaseModel):
+    ccsds_cdm_vers: float
+    creation_date: datetime
+    originator: str
+    message_id: int
+    tca: datetime
+    miss_distance: int
+    object_designator: int
+    catalog_name: str
+    object_name: str
+    international_designator: str
+    ephemeris_name: str
+    covariance_method: str
+    maneuverable: str
+    ref_frame: str
+    x: float
+    y: float
+    z: float
+    x_dot: float
+    y_dot: float
+    z_dot: float
+    cr_r: float
+    ct_r: float
+    ct_t: float
+    cn_r: float
+    cn_t: float
+    cn_n: float
+    crdot_r: float
+    crdot_t: float
+    crdot_n: float
